a086a41bb157856b14bf96b2242ccc94448ba175
BT_1N3_27@bktech.local <BT_1N3_27@bktech.local>
2018-11-27 11:11:32 -0500

    Initial commit

---
+++ src/com/company/method.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class method {
+
+    public static void swap(int[]arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        if (left<right)
+        {
+            int pivot = partition(arr, left, right);
+            quickSort(arr, left, pivot -1);
+            quickSort(arr, pivot+1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left-1;
+        for (int j = left; j<right;j++)
+        {
+            if (arr[j]<=pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, right);
+        return i+1;
+    }
+}
---
+++ src/com/company/runner.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class runner {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4};
+
+
+        method.quickSort(arr, 1, 3);
+    }
+}
